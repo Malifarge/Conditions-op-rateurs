9bcfa0a3bd967f3853245155633596747e109068
Thomas <malifarge.thomas9@gmail.com>
2022-06-21 11:20:24 +0200

    Condition

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ console.log(test > bis);
 console.log(test < bis);
 
 
+// 02 - Condition
+
+const limit = 50
+const score = 45
+
+if (score >= limit){
+    console.log("Ok good !");
+} else {
+    console.log("Oh nooo...");
+}
+
